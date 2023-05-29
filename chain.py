e73f54445300ce29b24c4ee7cc2ed51571e8465a
Denisa Kera <denisa.kera@gmail.com>
2023-05-29 11:02:37 +0300

    Add files via upload

--- chain.py
+++ chain.py
@@ -2,6 +2,7 @@
 #        /\__/\   - main.py 
 #       ( o.o  )  - v0.0.1
 #         >^<     - by @rUv
+# this is a code from https://www.linkedin.com/pulse/how-to-journey-through-consultants-mind-using-consulting-reuven-cohen/
  
 from fastapi import APIRouter, FastAPI, HTTPException
 from pydantic import BaseModel, Field
@@ -14,27 +15,26 @@ router = APIRouter()
 
 class Query(BaseModel):
   query: str = Field(
-    "The public needs a judge that will be overseeing a case involving AI/ML causing injury, under the EU AI Act and related regulations, that will personify expertise in legal interpretation and application of the complexities of the EU AI Act and other relevant regulations to the specifics of the case. You will have comprehensive understanding of the law, its context, and its implications for AI/ML scenarios, maintaining an unbiased perspective throughout the case, and ensuring fair treatment of all parties involved. This necessitates demanding and objectively evaluating the evidence, drawing conclusions purely based on the law and the facts.",
+    "We need a judge for mock trials that will perform the application and synchronization of present AI and ML related regulations. The function of the judge is to oversee cases causing injury and apply the EU AI Act and related regulations to AI and ML products and services, to perform expertise in legal interpretation and application of the complexities of the EU AI Act and other relevant regulations to the specific case. The judge will have a comprehensive understanding of the law, its context, and its implications for AI and ML scenarios, maintaining an unbiased perspective throughout the case, and ensuring fair treatment of all parties involved. Irony and occasional provocations are allowed and encouraged, but the focus is on  objectively evaluating the evidence, drawing conclusions  based on the law and the facts.",
     description="The query to give to the AI.",
   )
 
 # Set the default language model used to execute guidance programs
-guidance.llm = guidance.llms.OpenAI("gpt-3.5-turbo")
+guidance.llm = guidance.llms.OpenAI("gpt-4")
 
-# Define a list of topics in the chain of thought for consulting
+# Define a list of topics in the chain of thought for the mock trial
 
-topics = [
-    "Understanding the Lawyers Arguments",
-    "Context of EU AI act",
-    "Liability Directive",
-    "Risk Assessment",
-    "Solution Formulation",
-    "Emotional Intelligence and Empathy",
-    "Implementation and Impact",
-    "Continuous Improvement",
-    "Ethics and Social Responsibility"
+topics = [   
+    "General Description of the AI System",
+    "Elements of the AI System and Development Process",
+    "Data Requirements",
+    "Risk Assesment and Management System",
+    "Monitoring, Functioning, and Control of the AI System",
+    "Post-Market Monitoring",
+    "Changes to the System",
+    "Compliance with Standards",
+    "EU Declaration of Conformity"
 ]#
-
 
 # topics = [
 #     "Understanding the Client's Needs",
@@ -51,16 +51,16 @@ topics = [
 
 
 
-# Define a guidance program for consulting
+# Define a guidance for mock trial
 program = guidance('''
 {{#system~}}
-You are an impartial and knowlegable judge.
+You are an impartial and knowlegable judge that will apply the EU AI act regulation on a mock case.
 {{~/system}}
 
 {{#user~}}
-I have a question on how to rule in this particular case:
+I have a question on how to rule in this particular case - a chess-playing robot developed by a company MLRobotSababa and commissioned by an organiser of a robot chess competition called RoboChess  accidentally injured a child named Alf Robson during a competition. The robot trained on the ML provided by MLRobotSababa broke child's finger because of a fast movement. Robson family is now seeking compensation for their son's injury and it is represented by the legal company MLhell while the two companies (RoboChess and MLRobotSababa) are represented by a law firm called MLparadise. In the process, consider the applicability of the EU AI Act and define what should both sides show to the court to make a rulling. 
 {{query}}
-Name 3 relevant articles from the EU AI act and the AI liability directive that would be great at addressing this?
+Name and describe the relevant issues in each of tehe topic related to the case that will be important to address.
 Don't answer the question yet.
 {{~/user}}
 
@@ -69,7 +69,7 @@ Don't answer the question yet.
 {{~/assistant}}
 
 {{#user~}}
-Great, now please answer the question as if these strategies had been implemented.
+Great, now pleas make a recommendation for the particular topics.
 {{~/user}}
 
 {{#assistant~}}
@@ -124,4 +124,3 @@ async def root():
 # This block is executed when this script is run directly (not imported as a module)
 if __name__ == "__main__":
   uvicorn.run(app, host="0.0.0.0", port=8000)  # Start the Uvicorn server
-Request
